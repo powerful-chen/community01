4ba0fd2d1a5c6c34a729a3be71998bc1c4628841
小陈 <1716867307@qq.com>
2020-03-19 17:55:47 +0800

    commit

--- src/main/java/life/majiang/community/Controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/Controller/AuthorizeController.java
@@ -55,6 +55,7 @@ public class AuthorizeController {
             userMapper.insert(user);
             //登录成功，写cookie和session
             response.addCookie(new Cookie("token",token));
+
             return "redirect:/";
         }else {
             //登录失败，重新登录
